c233b9181b345ff473508796791e6e60107ff901
suryarkrishna <ramakrishna.koda.3@gmail.com>
2023-08-28 21:59:32 +0530

    in Progress

--- react-tiktactoe/tic-tac-toe/src/index.js
+++ react-tiktactoe/tic-tac-toe/src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Square = () => {
+  return (
+    <div className='square'>
+      X
+    </div>
+  )
+};
+
+const Board = () => {
+  const renderSquare = () => {
+    return (
+      <Square />
+    );
+  };
+
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20
+    }}>
+      Board
+      <div className='board-row'>
+      {renderSquare()}
+      {renderSquare()}
+      {renderSquare()}
+      </div>
+      <div className='board-row'>
+      {renderSquare()}
+      {renderSquare()}
+      {renderSquare()}
+      </div>
+      <div className='board-row'>
+      {renderSquare()}
+      {renderSquare()}
+      {renderSquare()}
+      </div>
+    </div>
+  )
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      Game
+      <Board />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
