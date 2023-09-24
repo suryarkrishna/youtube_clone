d7f026d932599b00bfcc505f6e9a1fc445f19e70
suryarkrishna <ramakrishna.koda.3@gmail.com>
2023-09-24 14:48:24 +0530

    updated

--- practice/regexNotes/script.js
+++
@@ -1,6 +0,0 @@
-var str = "This is a black ball pen. This pen is really smooth. I love this PEN.";
-// var pattern = /pen/gi;
-var pattern = new RegExp('pen', 'gi');
-console.log(str.search(pattern));
-console.log(pattern.test(str));
-console.log(str.match(pattern))
